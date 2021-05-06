4b8f62665d99a169af039cddfa159c46a44ea7e3
ontemehoje <pedrocp99@hotmail.com>
2021-05-05 23:02:12 -0300

    trabalho

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { City } from 'src/domain/entities/city';
+import { SearchCityService } from 'src/domain/services/search-city.service';
+import { Storage } from '@ionic/storage-angular';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  cities: City[];
+  hasError: boolean = false;
+  errorMessage: string;
+
+  constructor(
+    private readonly searchService: SearchCityService,
+    private readonly router: Router,
+    //private storage: Storage
+    
+    
+  ) {}
+
+  //async ngOnInit() {
+    // If using a custom driver:
+    // await this.storage.defineDriver(MyCustomDriver)
+    //await this.storage.create();
+  //}
+
+  async onSearch(query: string) {
+    try {
+      this.hasError = false;
+      this.cities = await this.searchService.search(query);
+    } catch (error) {
+      this.hasError = true;
+      this.errorMessage = error.message;
+    }
+  }
+
+  onSelectCity(cityId: string,cityName: string) {
+    this.router.navigateByUrl(`/weather/${cityId}`);
+    //await storage.set('key', 'cityName');
+  }
+
+}
+
